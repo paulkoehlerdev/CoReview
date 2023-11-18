72a72ce16cdce1b3ce26d337e2bfe5c9ef05fbb0
Paul Köhler <koehler_paul@outlook.de>
2023-11-18 01:09:19 +0100

    Submit Intellij Plugin Template

---
+++ .gradle/8.4/dependencies-accessors/5ff144de810dd952e050983bda2040fa7aa351f8/sources/org/gradle/accessors/dm/LibrariesForLibs.java
@@ -0,0 +1,159 @@
+package org.gradle.accessors.dm;
+
+import org.gradle.api.NonNullApi;
+import org.gradle.api.artifacts.MinimalExternalModuleDependency;
+import org.gradle.plugin.use.PluginDependency;
+import org.gradle.api.artifacts.ExternalModuleDependencyBundle;
+import org.gradle.api.artifacts.MutableVersionConstraint;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.ProviderFactory;
+import org.gradle.api.internal.catalog.AbstractExternalDependencyFactory;
+import org.gradle.api.internal.catalog.DefaultVersionCatalog;
+import java.util.Map;
+import org.gradle.api.internal.attributes.ImmutableAttributesFactory;
+import org.gradle.api.internal.artifacts.dsl.CapabilityNotationParser;
+import javax.inject.Inject;
+
+/**
+ * A catalog of dependencies accessible via the `libs` extension.
+ */
+@NonNullApi
+public class LibrariesForLibs extends AbstractExternalDependencyFactory {
+
+    private final AbstractExternalDependencyFactory owner = this;
+    private final VersionAccessors vaccForVersionAccessors = new VersionAccessors(providers, config);
+    private final BundleAccessors baccForBundleAccessors = new BundleAccessors(objects, providers, config, attributesFactory, capabilityNotationParser);
+    private final PluginAccessors paccForPluginAccessors = new PluginAccessors(providers, config);
+
+    @Inject
+    public LibrariesForLibs(DefaultVersionCatalog config, ProviderFactory providers, ObjectFactory objects, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) {
+        super(config, providers, objects, attributesFactory, capabilityNotationParser);
+    }
+
+        /**
+         * Creates a dependency provider for annotations (org.jetbrains:annotations)
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getAnnotations() {
+            return create("annotations");
+    }
+
+    /**
+     * Returns the group of versions at versions
+     */
+    public VersionAccessors getVersions() {
+        return vaccForVersionAccessors;
+    }
+
+    /**
+     * Returns the group of bundles at bundles
+     */
+    public BundleAccessors getBundles() {
+        return baccForBundleAccessors;
+    }
+
+    /**
+     * Returns the group of plugins at plugins
+     */
+    public PluginAccessors getPlugins() {
+        return paccForPluginAccessors;
+    }
+
+    public static class VersionAccessors extends VersionFactory  {
+
+        public VersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Returns the version associated to this alias: annotations (24.0.1)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getAnnotations() { return getVersion("annotations"); }
+
+            /**
+             * Returns the version associated to this alias: changelog (2.2.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getChangelog() { return getVersion("changelog"); }
+
+            /**
+             * Returns the version associated to this alias: gradleIntelliJPlugin (1.16.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getGradleIntelliJPlugin() { return getVersion("gradleIntelliJPlugin"); }
+
+            /**
+             * Returns the version associated to this alias: kotlin (1.9.10)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getKotlin() { return getVersion("kotlin"); }
+
+            /**
+             * Returns the version associated to this alias: kover (0.7.3)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getKover() { return getVersion("kover"); }
+
+            /**
+             * Returns the version associated to this alias: qodana (0.1.13)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getQodana() { return getVersion("qodana"); }
+
+    }
+
+    public static class BundleAccessors extends BundleFactory {
+
+        public BundleAccessors(ObjectFactory objects, ProviderFactory providers, DefaultVersionCatalog config, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) { super(objects, providers, config, attributesFactory, capabilityNotationParser); }
+
+    }
+
+    public static class PluginAccessors extends PluginFactory {
+
+        public PluginAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Creates a plugin provider for changelog to the plugin id 'org.jetbrains.changelog'
+             * This plugin was declared in catalog libs.versions.toml
+             */
+            public Provider<PluginDependency> getChangelog() { return createPlugin("changelog"); }
+
+            /**
+             * Creates a plugin provider for gradleIntelliJPlugin to the plugin id 'org.jetbrains.intellij'
+             * This plugin was declared in catalog libs.versions.toml
+             */
+            public Provider<PluginDependency> getGradleIntelliJPlugin() { return createPlugin("gradleIntelliJPlugin"); }
+
+            /**
+             * Creates a plugin provider for kotlin to the plugin id 'org.jetbrains.kotlin.jvm'
+             * This plugin was declared in catalog libs.versions.toml
+             */
+            public Provider<PluginDependency> getKotlin() { return createPlugin("kotlin"); }
+
+            /**
+             * Creates a plugin provider for kover to the plugin id 'org.jetbrains.kotlinx.kover'
+             * This plugin was declared in catalog libs.versions.toml
+             */
+            public Provider<PluginDependency> getKover() { return createPlugin("kover"); }
+
+            /**
+             * Creates a plugin provider for qodana to the plugin id 'org.jetbrains.qodana'
+             * This plugin was declared in catalog libs.versions.toml
+             */
+            public Provider<PluginDependency> getQodana() { return createPlugin("qodana"); }
+
+    }
+
+}
---
+++ .gradle/8.4/dependencies-accessors/5ff144de810dd952e050983bda2040fa7aa351f8/sources/org/gradle/accessors/dm/LibrariesForLibsInPluginsBlock.java
@@ -0,0 +1,169 @@
+package org.gradle.accessors.dm;
+
+import org.gradle.api.NonNullApi;
+import org.gradle.api.artifacts.MinimalExternalModuleDependency;
+import org.gradle.plugin.use.PluginDependency;
+import org.gradle.api.artifacts.ExternalModuleDependencyBundle;
+import org.gradle.api.artifacts.MutableVersionConstraint;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.ProviderFactory;
+import org.gradle.api.internal.catalog.AbstractExternalDependencyFactory;
+import org.gradle.api.internal.catalog.DefaultVersionCatalog;
+import java.util.Map;
+import org.gradle.api.internal.attributes.ImmutableAttributesFactory;
+import org.gradle.api.internal.artifacts.dsl.CapabilityNotationParser;
+import javax.inject.Inject;
+
+/**
+ * A catalog of dependencies accessible via the `libs` extension.
+ */
+@NonNullApi
+public class LibrariesForLibsInPluginsBlock extends AbstractExternalDependencyFactory {
+
+    private final AbstractExternalDependencyFactory owner = this;
+    private final VersionAccessors vaccForVersionAccessors = new VersionAccessors(providers, config);
+    private final BundleAccessors baccForBundleAccessors = new BundleAccessors(objects, providers, config, attributesFactory, capabilityNotationParser);
+    private final PluginAccessors paccForPluginAccessors = new PluginAccessors(providers, config);
+
+    @Inject
+    public LibrariesForLibsInPluginsBlock(DefaultVersionCatalog config, ProviderFactory providers, ObjectFactory objects, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) {
+        super(config, providers, objects, attributesFactory, capabilityNotationParser);
+    }
+
+        /**
+         * Creates a dependency provider for annotations (org.jetbrains:annotations)
+         * This dependency was declared in catalog libs.versions.toml
+     * @deprecated Will be removed in Gradle 9.0.
+         */
+    @Deprecated
+        public Provider<MinimalExternalModuleDependency> getAnnotations() {
+        org.gradle.internal.deprecation.DeprecationLogger.deprecateBehaviour("Accessing libraries or bundles from version catalogs in the plugins block.").withAdvice("Only use versions or plugins from catalogs in the plugins block.").willBeRemovedInGradle9().withUpgradeGuideSection(8, "kotlin_dsl_deprecated_catalogs_plugins_block").nagUser();
+            return create("annotations");
+    }
+
+    /**
+     * Returns the group of versions at versions
+     */
+    public VersionAccessors getVersions() {
+        return vaccForVersionAccessors;
+    }
+
+    /**
+     * Returns the group of bundles at bundles
+     * @deprecated Will be removed in Gradle 9.0.
+     */
+    @Deprecated
+    public BundleAccessors getBundles() {
+        org.gradle.internal.deprecation.DeprecationLogger.deprecateBehaviour("Accessing libraries or bundles from version catalogs in the plugins block.").withAdvice("Only use versions or plugins from catalogs in the plugins block.").willBeRemovedInGradle9().withUpgradeGuideSection(8, "kotlin_dsl_deprecated_catalogs_plugins_block").nagUser();
+        return baccForBundleAccessors;
+    }
+
+    /**
+     * Returns the group of plugins at plugins
+     */
+    public PluginAccessors getPlugins() {
+        return paccForPluginAccessors;
+    }
+
+    public static class VersionAccessors extends VersionFactory  {
+
+        public VersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Returns the version associated to this alias: annotations (24.0.1)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getAnnotations() { return getVersion("annotations"); }
+
+            /**
+             * Returns the version associated to this alias: changelog (2.2.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getChangelog() { return getVersion("changelog"); }
+
+            /**
+             * Returns the version associated to this alias: gradleIntelliJPlugin (1.16.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getGradleIntelliJPlugin() { return getVersion("gradleIntelliJPlugin"); }
+
+            /**
+             * Returns the version associated to this alias: kotlin (1.9.10)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getKotlin() { return getVersion("kotlin"); }
+
+            /**
+             * Returns the version associated to this alias: kover (0.7.3)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getKover() { return getVersion("kover"); }
+
+            /**
+             * Returns the version associated to this alias: qodana (0.1.13)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getQodana() { return getVersion("qodana"); }
+
+    }
+
+    /**
+     * @deprecated Will be removed in Gradle 9.0.
+     */
+    @Deprecated
+    public static class BundleAccessors extends BundleFactory {
+
+        public BundleAccessors(ObjectFactory objects, ProviderFactory providers, DefaultVersionCatalog config, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) { super(objects, providers, config, attributesFactory, capabilityNotationParser); }
+
+    }
+
+    public static class PluginAccessors extends PluginFactory {
+
+        public PluginAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Creates a plugin provider for changelog to the plugin id 'org.jetbrains.changelog'
+             * This plugin was declared in catalog libs.versions.toml
+             */
+            public Provider<PluginDependency> getChangelog() { return createPlugin("changelog"); }
+
+            /**
+             * Creates a plugin provider for gradleIntelliJPlugin to the plugin id 'org.jetbrains.intellij'
+             * This plugin was declared in catalog libs.versions.toml
+             */
+            public Provider<PluginDependency> getGradleIntelliJPlugin() { return createPlugin("gradleIntelliJPlugin"); }
+
+            /**
+             * Creates a plugin provider for kotlin to the plugin id 'org.jetbrains.kotlin.jvm'
+             * This plugin was declared in catalog libs.versions.toml
+             */
+            public Provider<PluginDependency> getKotlin() { return createPlugin("kotlin"); }
+
+            /**
+             * Creates a plugin provider for kover to the plugin id 'org.jetbrains.kotlinx.kover'
+             * This plugin was declared in catalog libs.versions.toml
+             */
+            public Provider<PluginDependency> getKover() { return createPlugin("kover"); }
+
+            /**
+             * Creates a plugin provider for qodana to the plugin id 'org.jetbrains.qodana'
+             * This plugin was declared in catalog libs.versions.toml
+             */
+            public Provider<PluginDependency> getQodana() { return createPlugin("qodana"); }
+
+    }
+
+}
